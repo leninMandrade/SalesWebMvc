083bba148351eaf601ef2205f13f42eab9b75fa0
Lênin Andrade <leninandrade.44@hotmail.com>
2022-09-16 22:34:53 -0300

    Implementing Simple Search

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using SalesWebMvc.Models.Enums;
 
 namespace SalesWebMvc.Models
@@ -8,6 +9,8 @@ namespace SalesWebMvc.Models
         public int Id { get; set; }
         public DateTime Date { get; set; }
         public double Amount { get; set; }
+
+        [Display(Name = "Sale Status")]
         public SaleStatus SaleStatus { get; set; }
         public Seller Seller { get; set; }
 
